8de238b263b877a7dade13b2e4f87f55ef6555f3
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-02-06 13:19:45 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config;
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Smart village server is Running');
+ });
+ 
+ app.listen(port, () => {
+    console.log('server is running in localhost:', port)
+ })
