67534dd0771d29f4e54f20669374172d718f6c85
MD-ABUSUFIAN <abusufian.programmer@gmail.com>
2022-03-01 23:26:29 +0600

    news section get & post method implement

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const corsOptions = require('./config/corsOptions');
 const authRoutes = require('./routes/authRoutes');
 const teacherRoutes = require('./routes/teacherRoutes');
 const notificationRoutes = require('./routes/NotificationRoutes');
+const newsRoutes=require('./routes/NewsRoutes')
 const errorhandler = require('./middlewares/errorhandler');
 
 // midlewire
@@ -36,6 +37,8 @@ async function run() {
     app.use('/auth', authRoutes);
     app.use('/teacher', teacherRoutes);
     app.use('/notification', notificationRoutes);
+    app.use('/news',newsRoutes);
+
   } catch (error) {
     console.log(error.message);
   }
