1c500b9aa3e683db9ad08874a4c5556a239996d9
coder71 <kamrunnaher877@gmail.com>
2022-03-09 13:57:23 +0600

    [ADD] blog filtering

--- controller/Education/studentController.js
+++ controller/Education/studentController.js
@@ -1,6 +1,7 @@
 const Video = require('../../schemas/Education/VideoSchema/Video');
 const Blog = require('../../schemas/Education/BlogSchema/Blog');
 const Roles = require('../../config/roles');
+const { filterBlogs } = require('../../utilities/Filter');
 const getallVideo = async (req, res, next) => {
   try {
     const videos = await Video.find();
@@ -11,39 +12,79 @@ const getallVideo = async (req, res, next) => {
 };
 
 const getallBlogs = async (req, res, next) => {
-  let { page, size, roles } = req.query;
-  console.log(page, size, roles);
-  roles = JSON.parse(roles);
-  console.log(roles);
+  let { page, size, roles, search } = req.query;
+  if (roles) {
+    roles = JSON.parse(roles);
+  }
+
   try {
     let count;
     const isAdmin = roles && roles.length > 0 && roles.includes(Roles.Admin);
-    if (!(page && size)) {
-      if (isAdmin) {
-        count = await Blog.count({});
+
+    if (isAdmin) {
+      if (search && page && size) {
+        const allBlogs = await Blog.find();
+        const allFilteredBlogs = filterBlogs(allBlogs, search);
+        count = allFilteredBlogs.length; // count will be only filtered blogs from all blogs
+        console.log('api hitted in admin');
+        const sendBlogs = parseInt(page)
+          ? parseInt(page) < parseInt(size)
+            ? allFilteredBlogs.slice(parseInt(page) * 10)
+            : allFilteredBlogs.slice(parseInt(page) * 10, parseInt(size))
+          : allFilteredBlogs.slice(parseInt(page), parseInt(size));
+
+        return res.json({
+          count,
+          blogs: sendBlogs,
+        });
       } else {
-        count = await Blog.count({ isVerified: true });
+        count = await Blog.count({}); // count will be all blogs
       }
     } else {
-      if (isAdmin) {
-        count = await Blog.count({});
+      if (search && page && size) {
+        const allVerifiedBlogs = await Blog.find({ isVerified: true });
+        const allVerifiedFilteredBlogs = filterBlogs(allVerifiedBlogs, search);
+        count = allVerifiedFilteredBlogs.length; // count will be all filtered blogs from filtered blogs
+        const sendBlogs = parseInt(page)
+          ? parseInt(page) < parseInt(size)
+            ? allVerifiedFilteredBlogs.slice(parseInt(page) * 10)
+            : allVerifiedFilteredBlogs.slice(
+                parseInt(page) * 10,
+                parseInt(size)
+              )
+          : allVerifiedFilteredBlogs.slice(parseInt(page), parseInt(size));
+        console.log('api hitted with search query');
+
+        return res.json({
+          count,
+          blogs: sendBlogs,
+        });
       } else {
-        count = await Blog.count({ isVerified: true });
+        count = await Blog.count({ isVerified: true }); // count will be all verified blogs
       }
     }
 
     let blogs;
-    if (roles && roles.length > 0 && roles.includes(Roles.Admin)) {
-      blogs = await Blog.find() // admin can access all blogs
-        .skip(parseInt(page) * parseInt(size))
-        .limit(parseInt(size));
+
+    if (isAdmin) {
+      if (page && size) {
+        blogs = await Blog.find() // admin can access all blogs
+          .skip(parseInt(page) * parseInt(size))
+          .limit(parseInt(size));
+      } else {
+        blogs = await Blog.find(); // send all if pagination query is not avialble
+      }
     } else {
-      blogs = await Blog.find({ isVerified: true }) // a normal user can only see blogs that are verified
-        .skip(parseInt(page) * parseInt(size))
-        .limit(parseInt(size));
+      if (page && size) {
+        blogs = await Blog.find({ isVerified: true })
+          .skip(parseInt(page) * parseInt(size))
+          .limit(parseInt(size)); // a normal user can only see blogs that are verified
+      } else {
+        blogs = await Blog.find({ isVerified: true }); // send all if pagination query is not avialble
+      }
     }
 
-    res.send({
+    res.json({
       count,
       blogs,
     });
