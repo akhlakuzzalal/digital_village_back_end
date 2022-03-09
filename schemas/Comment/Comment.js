ed9f785c7197f9e51472e04ba3e4f0d55c6d31a6
coder71 <kamrunnaher877@gmail.com>
2022-03-09 17:31:05 +0600

    [ADD] comment schema

---
+++ schemas/Comment/Comment.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = mongoose.Schema(
+  {
+    commenter: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    commentId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Video',
+    },
+    responseTo: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    comment: {
+      type: String,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+module.exports = mongoose.model('Comment', commentSchema);
