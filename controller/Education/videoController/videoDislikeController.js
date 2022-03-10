79884c07e910d3ef6777982e7b716830483a8098
coder71 <kamrunnaher877@gmail.com>
2022-03-10 23:18:33 +0600

    [ADD] like dislike controller

--- controller/Education/videoController/videoDislikeController.js
+++ controller/Education/videoController/videoDislikeController.js
@@ -1,4 +1,5 @@
 const VideoDisLike = require('../../../schemas/videoDislikeSchema/videoDislikeSchema');
+const videoLike = require('../../../schemas/VideoLikeSchema/VideoLikeSchema');
 
 const getVideoDisLikes = async (req, res, next) => {
   try {
@@ -19,6 +20,48 @@ const getVideoDisLikes = async (req, res, next) => {
   }
 };
 
+const addDisLike = async (req, res, next) => {
+  try {
+    let query = {};
+    if (videoId) {
+      query = { videoId, uId };
+    } else {
+      query = { commentId, uId };
+    }
+
+    const disLike = new VideoDisLike(query);
+    //save the like information data in MongoDB
+    disLike.save((err, dislikeResult) => {
+      if (err) return res.json({ success: false, err });
+
+      // if like button is allready clicked then decrease it
+      videoLike.findOneAndDelete(variable).exec((err, likeResult) => {
+        if (err) return res.status(400).json({ success: false, err });
+        res.status(200).json({ success: true });
+      });
+    });
+  } catch (error) {}
+};
+
+const removeDisLike = async (req, res, next) => {
+  try {
+    let query = {};
+    const { videoId, uId, commentId } = req.body;
+    if (videoId) {
+      query = { videoId, uId };
+    } else {
+      query = { commentId, uId };
+    }
+
+    VideoDisLike.findOneAndDelete(query).exec((err, result) => {
+      if (err) return res.status(400).json({ success: false, err });
+      res.status(200).json({ success: true });
+    });
+  } catch (error) {}
+};
+
 module.exports = {
   getVideoDisLikes,
+  addDisLike,
+  removeDisLike,
 };
