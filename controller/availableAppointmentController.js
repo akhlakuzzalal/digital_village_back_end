e80d6d9ec0d21fc0b5e3108f520f1b2585cf8ecc
Safia Jotey <safiaahmed1786.com>
2022-03-10 22:28:25 -0800

    [add] pdf functionality

--- controller/availableAppointmentController.js
+++ controller/availableAppointmentController.js
@@ -12,7 +12,8 @@ const handleSaveAppointment = async (req, res, next) => {
 };
 const handleGetAppointment = async (req, res, next) => {
   try {
-    const response = await AvailableAppointment.find();
+    const response = await AvailableAppointment.find({});
+    console.log(response);
     res.json(response);
   } catch (error) {
     next(error);
