daee1fc7149ddb569d3fc343f5ce9c79efe30801
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-03-17 22:48:25 +0600

    [MOD] socket io

--- routes/socialRoutes.js
+++ routes/socialRoutes.js
@@ -5,11 +5,13 @@ const {
   acceptFriend,
   cancleRequest,
 } = require('../controller/socialController');
+const { socketController } = require('../controller/SocketIOController');
 const router = express.Router();
 
 router.get('/allUsers', findAllUser);
 router.put('/request', requestFriend);
 router.put('/accept', acceptFriend);
 router.put('/cancel', cancleRequest);
+router.get('/message', socketController);
 
 module.exports = router;
