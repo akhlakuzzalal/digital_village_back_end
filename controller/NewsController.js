9e37553fb357b80a9ce0b182f4a6ab4baf023b9d
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-03-19 14:47:59 +0600

    [MOD] news

--- controller/NewsController.js
+++ controller/NewsController.js
@@ -31,7 +31,6 @@ const handleAddNews = async (req, res, next) => {
   }
 };
 
-
 const getSingleNews = async (req, res, next) => {
   try {
     const { id } = req.query;
@@ -53,20 +52,20 @@ const handleDeleteNews = async (req, res, next) => {
   }
 };
 
-const handleEditNews= async (req, res, next) => {
+const handleEditNews = async (req, res, next) => {
   const { id } = req.query;
-  console.log(req.file)
-  const file = {
-    name: req.file.originalname,
-    path: req.file.path,
-    type: req.file.mimetype,
-    size: fileSizeFormatter(req.file.size, 2), // 0.00
-  };
-
-  const editNews = {
-    ...JSON.parse(req.body.news),
-    bannerImg: file,
-  };
+  let editNews = {};
+  if (req.file) {
+    const file = {
+      name: req.file.originalname,
+      path: req.file.path,
+      type: req.file.mimetype,
+      size: fileSizeFormatter(req.file.size, 2), // 0.00
+    };
+    editNews = { ...JSON.parse(req.body.news), bannerImg: file };
+  } else {
+    editNews = { ...JSON.parse(req.body.news) };
+  }
 
   try {
     const response = await News.findOneAndUpdate({ _id: id }, editNews);
@@ -83,5 +82,5 @@ module.exports = {
   handleAddNews,
   handleDeleteNews,
   handleEditNews,
-  getSingleNews
+  getSingleNews,
 };
