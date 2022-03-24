1b935408b0a34a43eea56fa629ec026331f47109
akhlakuzzalal <akhlakuzzalal@gmail.com>
2022-03-25 04:16:41 +0600

    {ADD} sucess pAay

--- routes/eMarketRoutes.js
+++ routes/eMarketRoutes.js
@@ -6,6 +6,8 @@ const {
   updeteProduct,
   getAllMedicine,
   getProductsForAdmin,
+  addUserOrder,
+  getUserOrder,
 } = require('../controller/eMarketController');
 const validateUser = require('../middlewares/validateUser');
 const router = express.Router();
@@ -13,7 +15,9 @@ const router = express.Router();
 router.get('/products', getAllProducts);
 router.get('/medicines', getAllMedicine);
 router.get('/admin', getProductsForAdmin);
+router.get('/order', getUserOrder);
 router.post('/products', addProducts);
+router.post('/order', addUserOrder);
 router.delete('/delete/:id', deleteProduct);
 router.put('/update/:id', updeteProduct);
 
